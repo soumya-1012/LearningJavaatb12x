1bf3085be24071e0720dc6d088c6b066c2788f2e
Soumya-1012 <soumya.chava03@gmail.com>
2025-05-06 21:44:11 +0530

    commit

---
+++ src/ex_01_JavaBasics/HelloWorld.java
@@ -0,0 +1,8 @@
+package ex_01_JavaBasics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Soumya");
+
+    }
+}
---
+++ src/ex_01_JavaBasics/Lab001_Java.java
@@ -0,0 +1,7 @@
+package ex_01_JavaBasics;
+
+public class Lab001_Java {
+    public static void main(String[] args) {
+        System.out.println("Hi there");
+    }
+}
