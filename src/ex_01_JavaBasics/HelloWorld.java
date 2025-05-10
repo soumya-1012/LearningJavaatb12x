84a92ee8d6603f8716fe48834aaf8b059ea8493c
Soumya-1012 <soumya.chava03@gmail.com>
2025-05-10 11:13:48 +0530

    commit

--- src/ex_01_JavaBasics/HelloWorld.java
+++ src/ex_01_JavaBasics/HelloWorld.java
@@ -1,8 +1,17 @@
 package ex_01_JavaBasics;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main() {
         System.out.println("Hello Soumya");
+        System.out.println("");
+
+
+
+
+
+    }
+
+
 
     }
 }
---
+++ src/ex_01_JavaBasics/Lab002_Java.java
@@ -0,0 +1,43 @@
+package ex_01_JavaBasics;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Lab002_Java {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        float c=a/b;
+        float d=a%b;
+        System.out.println(c);
+        System.out.println(d);
+         a+=2;
+        System.out.println(a);
+         a-=2;
+         System.out.println(a);
+        a*=2;
+        System.out.println(a);
+        a/=2;
+        System.out.println(a);
+        a%=2;
+        System.out.println("a value is "+a);
+        System.out.println("1st value is" +a+b);
+       System.out.println("1st value is" +a-b);
+        System.out.println("1st value is" +a*b);
+        System.out.println("1st value is" +a/b);
+        System.out.println("1st value is" +a%b);
+        System.out.println("1st value is" +a+b);
+        System.out.println("10st value is" +-a+b);
+       // System.out.println("1st value is"+-a-b);
+        System.out.println("1st value is" +-a*b);
+        System.out.println("1st value is" +-a/b);
+        System.out.println("1st value is" +-a%b);
+        System.out.println("1st value is" +a+b);
+
+
+
+
+
+
+
+    }
+}
