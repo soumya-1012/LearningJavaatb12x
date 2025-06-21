555284587d76f20bb3fcdcf0f203c9e36fa6d4ca
Soumya-1012 <soumya.chava03@gmail.com>
2025-06-21 11:38:16 +0530

    Printf Task

---
+++ src/challenges/Operators.java
@@ -0,0 +1,41 @@
+package challenges;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        float c=a/b;
+        float d=a%b;
+        System.out.println(c);
+        System.out.println(d);
+         a+=2;
+        System.out.println(a);
+         a-=2;
+         System.out.println(a);
+        a*=2;
+        System.out.println(a);
+        a/=2;
+        System.out.println(a);
+        a%=2;
+        System.out.println("a value is "+a);
+        System.out.println("1st value is" +a+b);
+      // System.out.println("1st value is" +a-b);
+        System.out.println("1st value is" +a*b);
+        System.out.println("1st value is" +a/b);
+        System.out.println("1st value is" +a%b);
+        System.out.println("1st value is" +a+b);
+        System.out.println("10st value is" +-a+b);
+       // System.out.println("1st value is"+-a-b);
+        System.out.println("1st value is" +-a*b);
+        System.out.println("1st value is" +-a/b);
+        System.out.println("1st value is" +-a%b);
+        System.out.println("1st value is" +a+b);
+
+
+
+
+
+
+
+    }
+}
